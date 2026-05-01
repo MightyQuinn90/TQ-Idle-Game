c742c98e0938b131ba8df52d704a0280a1d2cf29
MightyQuinn90 <tequinn90@gmail.com>
2026-05-01 01:32:54 -0500

    Delete TQ Idle Game directory

--- TQ Idle Game/dist/Core/utils.js
+++
@@ -1,8 +0,0 @@
-export function getRandomItem(array) {
-    if (array.length === 0) {
-        throw new Error("Array Empty");
-    }
-    const index = Math.floor(Math.random() * array.length);
-    return array[index];
-}
-//# sourceMappingURL=utils.js.map
